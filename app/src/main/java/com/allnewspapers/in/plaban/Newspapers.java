08a3905f22e78e34b3e9882cf21c78ce36735f06
plabandas <turjoyplaban09@gmail.com>
2020-10-14 23:05:16 +0600

    BanglaTab and EnglishTab Items CLICKED

---
+++ app/src/main/java/com/allnewspapers/in/plaban/Newspapers.java
@@ -0,0 +1,18 @@
+package com.allnewspapers.in.plaban;
+
+public class Newspapers {
+
+    private int Photo ;
+
+    public Newspapers(int photo) {
+        Photo = photo;
+    }
+
+    public int getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(int photo) {
+        Photo = photo;
+    }
+}
---
+++ app/src/main/java/com/allnewspapers/in/plaban/RecyclerViewAdapterDemo_English.java
@@ -0,0 +1,79 @@
+package com.allnewspapers.in.plaban;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.List;
+
+public class RecyclerViewAdapterDemo_English extends RecyclerView.Adapter<RecyclerViewAdapterDemo_English.MyViewHolder> {
+
+    private Context mContext;
+    List<Newspapers> mdata;
+    int id;
+
+    public RecyclerViewAdapterDemo_English(Context mContext, List<Newspapers> mdata) {
+        this.mContext = mContext;
+        this.mdata = mdata;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v;
+        v = LayoutInflater.from(mContext).inflate(R.layout.single_row_recycler, parent, false);
+
+        final MyViewHolder vHolder = new MyViewHolder(v);
+
+        vHolder.item_cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                id = vHolder.getAdapterPosition();
+
+                Toast.makeText(mContext, "Button is Clicked : ID = " + String.valueOf(id), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return vHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.img.setImageResource(mdata.get(position).getPhoto());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        private ImageView img;
+        private CardView item_cardView;
+
+
+        public MyViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+            img = itemView.findViewById(R.id.newspaper_pic_demo);
+            item_cardView = itemView.findViewById(R.id.item_card_id);
+        }
+
+    }
+}
